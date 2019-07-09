9f17644c2a507bcfe7d08e472fe6380f7503c174
gmbrandt <gmbrandt@ucsb.edu>
2019-07-09 12:38:35 -0700

    new setup.py, working on allowing pip install easily

--- old_setup.py
+++ old_setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
+from Cython.Distutils import build_ext
 
-source_files = ['orbit.pyx']
-modules = [Extension("orbit", source_files, extra_compile_args=['-O3'])]
+ext_module = Extension("orbit",
+                       ['orbit.pyx'],
+                       extra_compile_args=['-O3'],
+)
 
 setup(
     name='orbit',
-    ext_modules=cythonize(modules),
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[ext_module],
     version='0.1.0',
     python_requires='>=3.5',
     install_requires=['numpy>=1.13', 'htof', 'emcee', 'Cython', 'pandas', 'astropy'],
