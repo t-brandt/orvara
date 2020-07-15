983a8fda5902b0f482e586fc72daa21c660b7a34
Timothy Brandt <tbrandt@ahab.physics.ucsb.edu>
2020-07-15 00:21:13 -0700

    Updated requirements: HTOF version, ptemcee

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='orbit3d',
       python_requires='>=3.5',
       package_dir={'orbit3d': 'orbit3d'},
       package_data={'orbit3d': ['data/*.fits']},
-      install_requires=['numpy>=1.13', 'htof>=0.1.3', 'emcee',
+      install_requires=['numpy>=1.13', 'htof>=0.2.10', 'emcee', 'ptemcee',
                         'Cython', 'pandas', 'astropy', 'pytest'],
       entry_points={'console_scripts': ['fit_orbit=orbit3d.main:run' ,'plot_orbit=orbit3d.main_plotting:run' ]})
