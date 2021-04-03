297be8a440ba8dce20809632a9b3e0e41e2cad5e
Timothy Brandt <tbrandt@ahab.physics.ucsb.edu>
2021-04-02 20:35:14 -0700

    Added -ffast-math flag to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 source_files = ['orbit3d/orbit.pyx']
-modules = [Extension("orbit3d.orbit", source_files, extra_compile_args=['-O3'])]
+modules = [Extension("orbit3d.orbit", source_files, extra_compile_args=['-O3', '-ffast-math'])]
 
 setup(name='orbit3d',
       ext_modules=cythonize(modules),
