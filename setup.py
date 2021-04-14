4853a532d1933d42d69be7ef1de8de1879cd79c0
Timothy Brandt <tbrandt@ahab.physics.ucsb.edu>
2021-04-13 22:23:45 -0700

    Renamed package

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-source_files = ['orbit3d/orbit.pyx']
-modules = [Extension("orbit3d.orbit", source_files, extra_compile_args=['-O3'])]
+source_files = ['orvara/orbit.pyx']
+modules = [Extension("orvara.orbit", source_files, extra_compile_args=['-O3'])]
 
-setup(name='orbit3d',
+setup(name='orvara',
       ext_modules=cythonize(modules),
       version='1.0.0',
       python_requires='>=3.5',
-      package_dir={'orbit3d': 'orbit3d'},
+      package_dir={'orvara': 'orvara'},
       install_requires=['numpy>=1.13', 'htof>=0.3.3', 'emcee', 'ptemcee',
                         'Cython', 'pandas', 'astropy', 'pytest', 'mock'],
-      entry_points={'console_scripts': ['fit_orbit=orbit3d.main:run', 'plot_orbit=orbit3d.main_plotting:run']})
+      entry_points={'console_scripts': ['fit_orbit=orvara.main:run', 'plot_orbit=orvara.main_plotting:run']})
