8a82271ca1a5eec80abfe9afc4538ac50d1c0f3a
Timothy Brandt <tbrandt@ahab.physics.ucsb.edu>
2021-04-14 17:18:01 -0700

    Changed nthreads to 1

--- orvara/tests/test_config.py
+++ orvara/tests/test_config.py
@@ -6,5 +6,5 @@ def test_example_config_file():
     parser.read('orvara/tests/config.ini')
     assert len(parser.items('data_paths')) == 8
     assert len(parser.items('mcmc_settings')) == 6
-    assert parser.getint('mcmc_settings', 'nthreads') == 2
+    assert parser.getint('mcmc_settings', 'nthreads') == 1
     assert parser.getboolean('mcmc_settings', 'use_epoch_astrometry')
